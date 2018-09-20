900d4521d509456eb89ac61a6daf65982b3f6df1
Kovalenko Alexey <alexey.s.russ@gmail.com>
2018-09-20 22:29:13 +0300

    Complete Block implementation

--- app/main.cpp
+++ app/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
-//#include "../libs/block.h"
+#include "../libs/block.h"
 
 using namespace std;
 
 
 int main(int argc, char**argv){
-    //Block<2, double> b({1, 2, 3, 4});
-    //cout << b << endl;
+    Block<2, double> b2({1, 2, 3, 4});
+    Block<2, double> b2_1({1, 2, 3, 4});
+    Block<3, double> b3({1, 2, 3, 4, 5, 6, 7, 8, 9});
+    cout << b3 * b3 << endl;
     return EXIT_SUCCESS;
 }
--- libs/block.h
+++ libs/block.h
@@ -19,9 +19,23 @@ private:
 public:
     Block();
     Block(const std::vector<T>&);
+    Block(const T*);
+    Block(const Block<block_size, T>&);
+
     ~Block();
+
     template <unsigned long _block_size, typename _T>
     friend std::ostream& operator<<(std::ostream&, const Block<_block_size, _T>&);
+
+    Block<block_size, T> operator=(const Block<block_size, T>&);
+
+    template <unsigned long _block_size, typename _T>
+    friend Block<_block_size, _T> operator+(const Block<_block_size, _T>&, const Block<_block_size, _T>&);
+
+    template <unsigned long _block_size, typename _T>
+    friend Block<_block_size, _T> operator*(const Block<_block_size, _T>&, const Block<_block_size, _T>&);
+
+    void zero();
 };
 
 
@@ -34,10 +48,12 @@ Block<block_size, T>::Block(){
     this->data = new T[block_size*block_size];
 }
 
+
 template<unsigned long block_size, typename T>
-Block<block_size, T>::~Block(){
-    delete[] this->data;
+Block<block_size, T>::Block(const T* V): Block(){
+    memcpy(this->data, V, sizeof(T)*block_size*block_size);
 }
+
 
 template<unsigned long block_size, typename T>
 Block<block_size, T>::Block(const std::vector<T> & v): Block(){
@@ -47,12 +63,60 @@ Block<block_size, T>::Block(const std::vector<T> & v): Block(){
     memcpy(this->data, v.data(), sizeof(T)*block_size*block_size);
 }
 
+
 template<unsigned long block_size, typename T>
-std::ostream& operator<<(std::ostream & os, const Block<block_size, T> & b) {
-    for (auto i = 0; i < block_size*block_size - 1; ++i)
-        os << b.data[i] << ' ';
-    os << b.data[block_size*block_size - 1];
+Block<block_size, T>::Block(const Block<block_size, T> & B) : Block(){
+    memcpy(this->data, B.data, sizeof(T)*block_size*block_size);
+}
+
+
+template<unsigned long block_size, typename T>
+Block<block_size, T>::~Block(){
+    delete[] this->data;
+}
+
+
+template<unsigned long block_size, typename T>
+std::ostream& operator<<(std::ostream & os, const Block<block_size, T> & B) {
+    for (auto i = 0; i < block_size; ++i) {
+        for (auto j = 0; j < block_size; ++j)
+            os << B.data[i*block_size + j] << ' ';
+        os << endl;
+    }
     return os;
+}
+
+
+template<unsigned long block_size, typename T>
+Block<block_size, T> Block<block_size, T>::operator=(const Block<block_size, T> & B) {
+    return Block<block_size, T>(B.data);
+}
+
+
+template<unsigned long _block_size, typename _T>
+Block<_block_size, _T> operator+(const Block<_block_size, _T> & A, const Block<_block_size, _T> & B) {
+    auto res = A;
+    for (auto i = 0; i < _block_size*_block_size; ++i)
+        res.data[i] += B.data[i];
+    return res;
+}
+
+template<unsigned long _block_size, typename _T>
+Block<_block_size, _T> operator*(const Block<_block_size, _T> & A, const Block<_block_size, _T> & B) {
+    Block<_block_size, _T> res;
+    res.zero();
+
+    for (auto i = 0; i < _block_size; ++i)
+        for (auto j = 0; j < _block_size; ++j)
+            for (auto k = 0; k < _block_size; ++k)
+                res.data[i*_block_size + j] += A.data[i*_block_size + k]*B.data[k*_block_size + j];
+
+    return res;
+}
+
+template<unsigned long block_size, typename T>
+void Block<block_size, T>::zero() {
+    memset(this->data, 0, sizeof(T)*block_size*block_size);
 }
 
 #endif //BLOCK_MATRIX_MULTIPLICATION_BLOCK_MATRIX_H
