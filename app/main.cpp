7b86bcb9647665e8469ce9fc9cd18d0be5bfef03
“AlexeySrus” <kovalenko@cvisionlab.com>
2018-11-13 01:15:49 +0300

    Update main

--- app/main.cpp
+++ app/main.cpp
@@ -12,17 +12,21 @@ int main(int argc, char**argv){
     auto timer = duration<double>();
 
     auto start_time = steady_clock::now();
-    Matrix<2, double> m("../../data/matrix_4.txt", BLOCK_LINE, false);
-    Matrix<2, double> m2("../../data/matrix_4.txt", BLOCK_COLUMN, false);
+    Matrix<24, double> m("../../data/matrix1.txt", BLOCK_LINE, false);
+    Matrix<24, double> m2("../../data/matrix2.txt", BLOCK_COLUMN, false);
     timer = steady_clock::now() - start_time;
 
-    cout << m << endl;
+    cout << "Load matrix time: " << timer.count() / 2 << " sec." << endl;
 
+    //cout << m << endl;
+
+    start_time = steady_clock::now();
     auto a = m*m2;
+    timer = steady_clock::now() - start_time;
 
-    cout << "Load matrix time: " << timer.count() << " sec." << endl;
+    cout << "Matrixes multiplication time: " << timer.count() << " sec." << endl;
 
-    cout << a << endl;
+    //cout << a << endl;
 
     return EXIT_SUCCESS;
 }
